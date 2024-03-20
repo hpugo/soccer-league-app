2a57b98b9718da50edaa8138b1e892f497cb7a2c
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-03-19 19:32:06 -0500

    adding header elements

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const user = {
+        name: 'John',
+    };
+    const logout = () => {};
+  
+    return (   
+        <header>
+            <div>
+                <Link to="/" className='logo'>
+                     <img src="https://seeklogo.com/images/S/soccer-club-emblem-logo-D69983F663-seeklogo.com.png" alt="League Logo"/>
+                </Link>
+                <nav>
+                    <ul>
+                    {
+                        user ? (
+                        <li className='container'>
+                            <Link to='/profile'>{user.name}</Link>
+                            <div className='profile-menu'>
+                                <Link to='/profile'>Profile</Link>
+                                <Link to='/fixtures'>Matches</Link>
+                                <a onClick={logout}>Logout</a>
+                            </div>
+                        </li> 
+                        ) :(
+                        <Link to='/login'>Login</Link>
+                        )}
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 }
