7d6bfaf8a55c70614069da2ceab51c75f3c9534d
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-03-19 21:20:12 -0500

    finished with header styling

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import '../Header/header.css'
 
 export default function Header() {
     const user = {
@@ -8,16 +9,28 @@ export default function Header() {
     const logout = () => {};
   
     return (   
-        <header>
-            <div>
-                <Link to="/" className='logo'>
+        <header className='header-container'>
+            <div className='container'>
+                <div className='logo'>
+                <Link to="/">
                      <img src="https://seeklogo.com/images/S/soccer-club-emblem-logo-D69983F663-seeklogo.com.png" alt="League Logo"/>
                 </Link>
-                <nav>
+                     <h1>Henry's Soccer League</h1>
+                </div>
+                <nav className='nav-bar'>
                     <ul>
+                        <li>
+                            <Link to='/league'>League</Link>
+                        </li>
+                        <li>
+                            <Link to='/teams'>Teams</Link>
+                        </li>
+                        <li>
+                            <Link to='/photos'>Photos</Link>
+                        </li>
                     {
                         user ? (
-                        <li className='container'>
+                        <li className='menu-container'>
                             <Link to='/profile'>{user.name}</Link>
                             <div className='profile-menu'>
                                 <Link to='/profile'>Profile</Link>
