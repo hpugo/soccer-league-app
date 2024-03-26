fa89b54a92cf3f59136fc04f7cef30792003e118
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-03-26 18:12:16 -0500

    adding homepage styling

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './css/homepage/homepage.css'
 
 export default function HomePage() {
   return (
     <div>
-      HomePage
+      <div className='main-page-container'>
+        <img src='../photos/New-3G_0.jpg' alt='Corner Side Football Pitch' />
+        <h1> The Best Recreational League! </h1>
+      </div>
+      <div className='info'>
+        <div className='website-league-info'>
+          <h2>Get Ready to Play!</h2>
+          <p>Welcome to Henry's Soccer League. A competetive league local to you!
+          
+          </p>
+        </div>
+        
+      </div>
+
     </div>
+   
   )
 }
